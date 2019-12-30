4ee5022540811cd68b996f44de030ee9bf1cb7b8
Devair Luiz Moitim <dmoitim@gmail.com>
2019-12-30 18:33:27 -0300

    Aula 05

---
+++ 01_fundamentos_basicos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fundamentos básicos</title>
+</head>
+
+<body>
+    <?php
+    // Comentário de uma linha
+
+    /* Comentário de
+       múltiplas linhas */
+    echo "Meu nome é Devair.<br>";
+    print "Minha idade é 35.";
+    ?>
+</body>
+
+</html>
