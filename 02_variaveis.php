0c78f1a399f51d4f892a0b2dec6cb5c4a530f04e
Devair Luiz Moitim <dmoitim@gmail.com>
2019-12-30 21:14:58 -0300

    Aula 06

---
+++ 02_variaveis.php
@@ -0,0 +1,41 @@
+<?php
+// Meus dados
+$nome = "Devair";
+$idade = 35;
+$altura = 1.81;
+
+echo "<p>Meu nomé é $nome, minha idade é $idade e minha altura é $altura.</p>";
+
+// Alterando meus dados
+$nome = "Ana";
+$idade = 14;
+$altura = 1.70;
+
+echo "<p>Meu nomé é $nome, minha idade é $idade e minha altura é $altura.</p>";
+
+echo "<hr>";
+
+$carro = "Hilux";
+$Carro = "Fusca";
+echo "<p>$Carro</p>";
+
+$nome = "Juca";
+$Nome = "DevA";
+echo "<p>$Nome</p>";
+
+// Regras para nomes de variáveis
+$variavel = "Começar com letras.";
+$_variavel = "Começar com underine.";
+$variavel1 = "Possuir números após a primeira letra.";
+$variavel_variavel = "Possuir underine entre as letra.";
+$variavelVariavel = "Possuir letras minúsculas e maiúsculas.";
+
+// Nomes inválidos
+/*
+$123 = "Não pode iniciar com números.";
+$varia vel "Não pode possuir espaços.";
+$v@riavel "Não pode possuir caracteres especiais.";
+*/
+
+
+// As variáveis são case sensitive, ou seja, $nome é diferente de $Nome
