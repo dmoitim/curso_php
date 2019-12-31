0d1ed7b153bdcbdec336c4d980fb56d7dfd3ad21
Devair Luiz Moitim <dmoitim@gmail.com>
2019-12-31 09:05:20 -0300

    Aula 07

---
+++ 03_variaveis_dinamicas.php
@@ -0,0 +1,13 @@
+<?php
+// Variáveis dinâmicas
+$bebida = "refrigerante";
+$$bebida = "guaraná";
+echo "<p>$bebida</p>";
+echo "<p>$refrigerante</p>";
+
+echo "<br>";
+
+$destino = "cidade";
+$$destino = "Ilhéus";
+echo "<p>$destino</p>";
+echo "<p>$cidade</p>";
