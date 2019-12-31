d9419d2ed07a94df4b0aca84f2eab32683451354
Devair Luiz Moitim <dmoitim@gmail.com>
2019-12-31 09:27:21 -0300

    Aula 09

---
+++ 05_aspas_concatenacao.php
@@ -0,0 +1,6 @@
+<?php
+$nome = "Devair";
+
+echo '<p>Meu nome é $nome.</p>';
+echo '<p>Meu nome é ' . $nome . ' e meu apelido \'DevA\'.</p>';
+echo "<p>Meu nome é $nome e meu apelido \"DevA\".</p>";
