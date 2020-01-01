0c3745e37f57ca6002074e740592fc77adfdf7d7
Devair Luiz Moitim <dmoitim@gmail.com>
2020-01-01 10:46:11 -0300

    Aula 12

---
+++ 08_arrays.php
@@ -0,0 +1,18 @@
+<?php
+$carros = array("BMW", "Veloster", "Hilux");
+$carros[] = "Amarok";
+$carros[10] = "Camaro";
+print_r($carros);
+
+echo "<hr>";
+
+$motos = array();
+$motos[] = "Yamaha";
+$motos[] = "Honda";
+$motos[5] = "Suzuki";
+print_r($motos);
+
+echo "<hr>";
+
+$clientes = ["Ana", "Bianca", "Devair"];
+print_r($clientes);
