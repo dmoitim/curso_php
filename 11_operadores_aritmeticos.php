0105225ff5dbe7610c39e95bfb6d8fc937974cbc
Devair Luiz Moitim <dmoitim@gmail.com>
2020-01-03 09:38:51 -0300

    Aula 21

---
+++ 11_operadores_aritmeticos.php
@@ -0,0 +1,47 @@
+<?php
+echo 10 + 10; // Adição
+echo "<hr>";
+echo 15 - 5;  // Subtração
+echo "<hr>";
+echo 3 * 5;   // Multiplicação
+echo "<hr>";
+echo 15 / 5;  // Divisão
+echo "<hr>";
+echo 15 % 5;  // Módulo
+echo "<hr>";
+echo 3 ** 5;  // Exponenciação
+echo "<hr>";
+
+// Trabalhando com variáveis
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 5;
+$e = 16;
+
+$adicao = $a + $b;
+echo $adicao;
+echo "<hr>";
+
+$subtracao = $c - $a;
+echo $subtracao;
+echo "<hr>";
+
+$multiplicacao = $d * $a;
+echo $multiplicacao;
+echo "<hr>";
+
+$divisao = $c / $a;
+echo $divisao;
+echo "<hr>";
+
+$modulo = $e % $d;
+echo $modulo;
+echo "<hr>";
+
+$exponenciacao = $a ** $d;
+echo $exponenciacao;
+echo "<hr>";
+
+// Prioridades
+echo (5 + 7 + 9 + 8) / 4;
