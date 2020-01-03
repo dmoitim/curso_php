d8fb1c39710a74a42c4624aa4d57ae2c84804c13
Devair Luiz Moitim <dmoitim@gmail.com>
2020-01-03 09:51:17 -0300

    Aula 23

---
+++ 13_operadores_de_atribuicao.php
@@ -0,0 +1,46 @@
+<?php
+$a = 10; // Atribuição
+echo $a;
+
+echo "<hr>";
+
+$b = 5;
+$a = $a + $b; // Atribuição de expressão aritmetica
+echo $a;
+
+echo "<hr>";
+
+$a = 10;
+$b = 5;
+$a += $b; // Atribuição cobinando operador aritmetico e atribuição
+echo $a;
+
+echo "<hr>";
+
+$a = 10;
+$b = 5;
+$a -= $b; // Atribuição cobinando operador aritmetico e atribuição
+echo $a;
+
+echo "<hr>";
+
+$a = 10;
+$b = 5;
+$a *= $b; // Atribuição cobinando operador aritmetico e atribuição
+echo $a;
+
+echo "<hr>";
+
+$a = 10;
+$b = 5;
+$a /= $b; // Atribuição cobinando operador aritmetico e atribuição
+echo $a;
+
+echo "<hr>";
+
+$a = 10;
+$b = 4;
+$a %= $b; // Atribuição cobinando operador aritmetico e atribuição
+echo $a;
+
+echo "<hr>";
