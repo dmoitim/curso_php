0538f7ed7ca0baa7d4262bb8523c0c99d1759e61
Devair Luiz Moitim <dmoitim@gmail.com>
2020-01-03 09:18:32 -0300

    Aula 19

---
+++ 10_condicionais.php
@@ -0,0 +1,16 @@
+<?php
+$numero = 50;
+
+if ($numero == 10) :
+    echo "É igual a 10.";
+elseif ($numero <= 9) :
+    echo "É menor ou igual a 9.";
+else :
+    echo "É diferente de 10.";
+endif;
+
+echo "<hr>";
+
+$media = 7;
+
+echo ($media >= 7 ? "Aprovado" : "Reprovado");
