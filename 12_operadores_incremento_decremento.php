c548d5ae8a50eecc8d9e6d349515f481eb8743b9
Devair Luiz Moitim <dmoitim@gmail.com>
2020-01-03 09:44:57 -0300

    Aula 22

---
+++ 12_operadores_incremento_decremento.php
@@ -0,0 +1,24 @@
+<?php
+$valor = 20;
+
+// Pré-Incremento
+echo "Pré-incremento: " . ++$valor;
+echo "<hr>";
+
+// Pós-Incremento
+echo "Pós-incremento, continuou com: " . $valor++;
+echo "<br>";
+echo "Imprimindo valor novamente: " . $valor;
+echo "<hr>";
+
+$valor = 20;
+
+// Pré-Decremento
+echo "Pré-decremento: " . --$valor;
+echo "<hr>";
+
+// Pós-Decremento
+echo "Pós-dencremento, continuou com: " . $valor--;
+echo "<br>";
+echo "Imprimindo valor novamente: " . $valor;
+echo "<hr>";
